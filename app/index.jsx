330416c672057c289f70b00b1ced201b1682f073
Disha Saraiya <saraiya.disha18@gmail.com>
2024-06-27 17:41:30 -0500

    feat: adds onboarding screen and custom button

--- app/index.jsx
+++ app/index.jsx
@@ -1,14 +1,62 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
-import { Link } from 'expo-router';
+import { ScrollView, Text, View, Image } from 'react-native';
+import { router } from 'expo-router';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { images } from '../constants';
+import CustomButton from '../components/custom-button';
 
-//Homepage
+/* 
+This is the onboarding page. 
+
+SafeAreaView: use this to make sure app is compatible on multiple mobile devices, 
+              this will keep the physical features of the device in mind.
+ScrollView: use this to make sure that even phones with smaller screens can see your whole content
+            using a scrollable container
+StatusBar: decides whether you want to show or hide your phone's top status bar in the application.
+           since our app is in dark mode, we set the style to light and bg colour to match the app's primary bg color
+View: equivalent to a <div>
+*/
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl font-pblack">Aora, our new app!</Text>
-      <StatusBar style="auto" />
-      <Link href="/home" style={{color: 'blue'}}> go to home </Link>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView contentContainerStyle={{ height: '100%' }}>
+        <View className="w-full justify-center items-center min-h-[85vh] px-4">
+          <Image
+            source={images.logo}
+            className="w-[130px] h-[84px]"
+            resizeMode="contain"
+          />
+          <Image
+            source={images.cards}
+            className="max-w-[380px] w-full h-[300px]"
+            resizeMode="contain"
+          />
+          <View className="relative-mt-5">
+            <Text className="text-3xl text-white font-bold text-center">
+              {' '}
+              Discover endless possibilities with{' '}
+              <Text className="text-secondary-200">Aora</Text>
+            </Text>
+            <Image
+              source={images.path}
+              className="w-[136px] h-[15px] absolute -bottom-3 -right-10"
+              resizeMode="contain"
+            />
+          </View>
+          <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">
+            Where creativity meets innovation: embark on a journey of
+            limitless exploration with Aora
+          </Text>
+          <CustomButton
+            title="Continue with Email"
+            handlePress={() => {
+              router.push('/sign-in');
+            }}
+            containerStyles="w-full mt-7"
+          />
+        </View>
+      </ScrollView>
+      <StatusBar backgroundColor="#161622" style="light" />
+    </SafeAreaView>
   );
 }
